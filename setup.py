ebe0c9ee3e087babb4b77da46b11d2e66c15f2ad
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-06 22:38:21 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='yt-time-count',
+    version='1.0',
+    description='Count the number and total duration of videos on a YouTube channel.',
+    long_description=open('README.md').read(),
+    author='Abhishek Kumar',
+    
+    scripts=['yt-time-count']
+)
