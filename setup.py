93e88fd51b13efd2aa16217061246d51304c127d
abhi-kr-2100 <66527624+abhi-kr-2100@users.noreply.github.com>
2021-12-16 18:56:50 +0530

    Use semantic versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='yt-time-count',
-    version='1.0',
+    version='1.0.0',
     description='Count the number and total duration of videos on a YouTube channel.',
     long_description=open('README.md').read(),
     author='Abhishek Kumar',
